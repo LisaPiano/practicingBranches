3f999c41161985962f52f793360e6c78c49844b6
Lisa Walters <lisacampiwalters@gmail.com>
2023-02-11 16:57:58 -0700

    Made some changes on bugFix branch

--- branching.js
+++ branching.js
@@ -1,2 +1,3 @@
 console.log("hello branching");
-console.log("Well, now I'm committing changes from my FeatureBranch, a brand new branch that is seperate from Main branch!");
+console.log("Well, now I'm committing changes from my FeatureBranch, a brand new branch that is seperate from Main branch!");g
+console.log("now, I'm going to make some changes from another branch, which is called 'bugFix'!");
